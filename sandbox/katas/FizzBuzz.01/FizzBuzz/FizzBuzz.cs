6d428d68c94d89671c30e9f5708b9484143e096f
Lubos Klauber <Klauberl@seznam.cz>
2025-09-23 20:47:29 +0200

    Fizzbuzz fix

--- sandbox/katas/FizzBuzz.01/FizzBuzz/FizzBuzz.cs
+++ sandbox/katas/FizzBuzz.01/FizzBuzz/FizzBuzz.cs
@@ -4,25 +4,22 @@ public class FizzBuzz
     {
         for (int i = 1; i <= lastNumber; i++)
         {
-            if (i % 3 == 0)
-            {
-                Console.WriteLine("Fizz");
-            }
-
-            if (i % 5 == 0)
-            {
-                Console.WriteLine("Buzz");
-            }
-
             if (i % 3 == 0 && i % 5 == 0)
             {
                 Console.WriteLine("FizzBuzz");
             }
-
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
                 Console.WriteLine(i);
-
-
-
+            }
         }
     }
 }
