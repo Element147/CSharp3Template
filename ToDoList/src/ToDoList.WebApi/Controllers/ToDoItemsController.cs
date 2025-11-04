517e0aea2c4670e9900342b00da4d93b6ba8b30c
Lubos Klauber <Klauberl@seznam.cz>
2025-11-04 18:22:55 +0100

    Sync with live lecture

--- ToDoList/src/ToDoList.WebApi/Controllers/ToDoItemsController.cs
+++ ToDoList/src/ToDoList.WebApi/Controllers/ToDoItemsController.cs
@@ -9,12 +9,11 @@ using ToDoList.Persistence;
 [ApiController]
 public class ToDoItemsController : ControllerBase
 {
-    public readonly List<ToDoItem> items = [];
-    private readonly ToDoItemsContext dbContext;
+    private readonly ToDoItemsContext context;
 
-    public ToDoItemsController(ToDoItemsContext dbContext)
+    public ToDoItemsController(ToDoItemsContext context)
     {
-        this.dbContext = dbContext;
+        this.context = context;
     }
 
     [HttpPost]
@@ -26,8 +25,8 @@ public class ToDoItemsController : ControllerBase
         //try to create an item
         try
         {
-            dbContext.ToDoItems.Add(item);
-            dbContext.SaveChanges();
+            context.ToDoItems.Add(item);
+            context.SaveChanges();
         }
         catch (Exception ex)
         {
@@ -47,7 +46,7 @@ public class ToDoItemsController : ControllerBase
         List<ToDoItem> itemsToGet;
         try
         {
-            itemsToGet = dbContext.ToDoItems.ToList();
+            itemsToGet = context.ToDoItems.ToList();
         }
         catch (Exception ex)
         {
@@ -67,9 +66,7 @@ public class ToDoItemsController : ControllerBase
         ToDoItem? itemToGet;
         try
         {
-            itemToGet = dbContext.ToDoItems.Find(toDoItemId);
-
-            //itemToGet = items.Find(i => i.ToDoItemId == toDoItemId);
+            itemToGet = context.ToDoItems.Find(toDoItemId);
         }
         catch (Exception ex)
         {
@@ -92,16 +89,18 @@ public class ToDoItemsController : ControllerBase
         try
         {
             //retrieve the item
-            var result = dbContext.ToDoItems.Update(updatedItem);
-            dbContext.SaveChanges();
-            /*
-            var itemIndexToUpdate = items.FindIndex(i => i.ToDoItemId == toDoItemId);
-            if (itemIndexToUpdate == -1)
+            var itemToUpdate = context.ToDoItems.Find(toDoItemId);
+            if (itemToUpdate is null)
             {
                 return NotFound(); //404
             }
-            updatedItem.ToDoItemId = toDoItemId;
-            items[itemIndexToUpdate] = updatedItem;*/
+
+            // Update the entity properties
+            itemToUpdate.Name = updatedItem.Name;
+            itemToUpdate.Description = updatedItem.Description;
+            itemToUpdate.IsCompleted = updatedItem.IsCompleted;
+
+            context.SaveChanges();
         }
         catch (Exception ex)
         {
@@ -118,15 +117,13 @@ public class ToDoItemsController : ControllerBase
         //try to delete the item
         try
         {
-            dbContext.ToDoItems.Remove(new ToDoItem { ToDoItemId = toDoItemId });
-            dbContext.SaveChanges();
-            /*
-            var itemToDelete = items.Find(i => i.ToDoItemId == toDoItemId);
+            var itemToDelete = context.ToDoItems.Find(toDoItemId);
             if (itemToDelete is null)
             {
                 return NotFound(); //404
             }
-            items.Remove(itemToDelete);*/
+            context.ToDoItems.Remove(itemToDelete);
+            context.SaveChanges();
         }
         catch (Exception ex)
         {
@@ -135,10 +132,5 @@ public class ToDoItemsController : ControllerBase
 
         //respond to client
         return NoContent(); //204
-    }
-
-    public void AddItemToStorage(ToDoItem item)
-    {
-        items.Add(item);
     }
 }
